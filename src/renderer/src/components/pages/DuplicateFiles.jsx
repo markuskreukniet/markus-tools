0494921beb78a7aa70632b83f7fa8cc3daaebdf6
markuskreukniet <shonof@gmail.com>
2023-04-06 13:47:56 +0200

    WIP

--- src/renderer/src/components/pages/DuplicateFiles.jsx
+++ src/renderer/src/components/pages/DuplicateFiles.jsx
@@ -1,14 +1,12 @@
 import FileOrFolderInput from '../FileOrFolderInput'
+import Page from '../Page'
 
 export default function DuplicateFiles() {
   return (
-    <div>
-      <h1>Duplicate Files Finder</h1>
-
+    <Page title="Duplicate Files Finder">
       <FileOrFolderInput onChange={(e) => console.log('e', e)} />
-
       <h2>result:</h2>
       <textarea rows="8" cols="55" />
-    </div>
+    </Page>
   )
 }
