cf7278721ed05a96680b1249aa5a0baf9d01808a
markuskreukniet <shonof@gmail.com>
2023-04-06 14:31:39 +0200

    WIP

--- src/renderer/src/components/FileSelector.jsx
+++ src/renderer/src/components/FileSelector.jsx
@@ -1,10 +1,21 @@
 export default function FileSelector(props) {
+  function clickInput(e) {
+    const input = e.srcElement.parentElement.getElementsByTagName('input')[0]
+    input.click()
+  }
+
   return (
-    <input
-      type="file"
-      webkitdirectory={props.folder}
-      onClick={(e) => (e.target.value = '') /* makes selecting the same file or folder possible */}
-      onChange={(e) => props.onChange(e.target.files)}
-    />
+    <div>
+      <input
+        type="file"
+        webkitdirectory={props.folder}
+        onClick={
+          (e) => (e.target.value = '') /* makes selecting the same file or folder possible */
+        }
+        onChange={(e) => props.onChange(e.target.files)}
+        class="display-none"
+      />
+      <button onClick={clickInput}>{`add a ${props.folder ? 'folder' : 'file'}`}</button>
+    </div>
   )
 }
