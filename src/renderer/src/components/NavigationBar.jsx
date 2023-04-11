41f6de52c1aebb41f903d6e336e468038f39250f
markuskreukniet <shonof@gmail.com>
2023-04-11 11:58:11 +0200

    updated console log

--- src/renderer/src/components/NavigationBar.jsx
+++ src/renderer/src/components/NavigationBar.jsx
@@ -4,9 +4,7 @@ export default function NavigationBar(props) {
   return (
     <ul id="navigation-bar">
       <For each={props.items}>
-        {(item) => (
-          <li onClick={() => console.log('set active css class and add pointer')}>{item}</li>
-        )}
+        {(item) => <li onClick={() => console.log('set active css class')}>{item}</li>}
       </For>
     </ul>
   )
