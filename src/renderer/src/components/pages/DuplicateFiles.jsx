9210ff27c50ae16835798cf01479d3a055ace2f2
markuskreukniet <shonof@gmail.com>
2023-04-11 11:11:44 +0200

    custom textarea placeholder

--- src/renderer/src/components/pages/DuplicateFiles.jsx
+++ src/renderer/src/components/pages/DuplicateFiles.jsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js'
+import { createSignal, Show } from 'solid-js'
 import FileOrFolderInput from '../FileOrFolderInput'
 import Page from '../Page'
 
@@ -17,11 +17,24 @@ export default function DuplicateFiles(props) {
     <Page title="Duplicate Files Finder">
       <FileOrFolderInput onChange={handleFilePaths} />
       <h2>result:</h2>
-      <textarea
-        readonly
-        value={duplicateFiles()}
-        placeholder="Add at least two files or a folder with two files and press 'submit.' Adding a folder also adds the files of its subfolders (its whole folder tree). The more files a folder has, the more time it can take to add the files, which can be noticeable. Also, the more files we add, the longer it takes to find duplicate files."
-      />
+
+      <Show
+        when={duplicateFiles() !== ''}
+        fallback={
+          <div class="custom-textarea-placeholder">
+            Add at least two files or a folder with two files and press 'submit.'
+            <br />
+            <br />
+            Adding a folder also adds the files of its subfolders (its whole folder tree).
+            <br />
+            <br />
+            The more files a folder has, the more time it can take to add the files, which can be
+            noticeable. Also, the more files we add, the longer it takes to find duplicate files.
+          </div>
+        }
+      >
+        <textarea readonly value={duplicateFiles()} placeholder="" />
+      </Show>
     </Page>
   )
 }
