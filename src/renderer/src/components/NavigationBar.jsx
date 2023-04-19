ee9bebe3436dd30c392046d33d93b1a360d9ee88
markuskreukniet <shonof@gmail.com>
2023-04-19 13:18:26 +0200

    WIP

--- src/renderer/src/components/NavigationBar.jsx
+++ src/renderer/src/components/NavigationBar.jsx
@@ -1,10 +1,32 @@
-import { For } from 'solid-js'
+import { createEffect, createSignal, For } from 'solid-js'
 
 export default function NavigationBar(props) {
+  const [items, setItems] = createSignal([])
+  createEffect(() => {
+    const activeItem = props.activeItem ? props.activeItem : props.items[0]
+
+    const newItems = new Array(props.items.length)
+    for (let i = 0; i < newItems.length; i++) {
+      const newItem = {
+        name: props.items[i],
+        active: props.items[i] === activeItem ? true : false
+      }
+      newItems[i] = newItem
+    }
+    setItems(newItems)
+  })
+
   return (
     <ul id="navigation-bar">
-      <For each={props.items}>
-        {(item) => <li onClick={() => console.log('set active css class')}>{item}</li>}
+      <For each={items()}>
+        {(item) => (
+          <li
+            onClick={() => console.log('set active css class')}
+            classList={{ navigationBarItemActive: item.active }}
+          >
+            {item.name}
+          </li>
+        )}
       </For>
     </ul>
   )
