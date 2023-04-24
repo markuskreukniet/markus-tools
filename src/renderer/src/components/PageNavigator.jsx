16cb72d7b3efcfede36a9cbc12df93701fb95b54
markuskreukniet <shonof@gmail.com>
2023-04-24 17:48:44 +0200

    WIP

--- src/renderer/src/components/PageNavigator.jsx
+++ src/renderer/src/components/PageNavigator.jsx
@@ -4,28 +4,40 @@ import NavigationBar from './NavigationBar'
 export default function PageNavigator(props) {
   const [navigationBarItems, setNavigationBarItems] = createSignal([])
 
+  // TODO: check if createEffect works when props.activeNavigationBarItem changes
   createEffect(() => {
-    const activeNavigationBarItem = props.activeNavigationBarItem
-      ? props.activeNavigationBarItem
-      : props.items[0]
+    if (props.activeNavigationBarItem) {
+      setState(props.navigationBarItemPageCombinations, props.activeNavigationBarItem)
+    } else {
+      setState(
+        props.navigationBarItemPageCombinations,
+        props.navigationBarItemPageCombinations[0].navigationBarItem
+      )
+    }
+  })
 
-    const newNavigationBarItems = new Array(props.navigationBarItemPageCombinations.length)
+  function setState(navigationBarItemPageCombinations, activeNavigationBarItem) {
+    const newNavigationBarItems = new Array(navigationBarItemPageCombinations.length)
     for (let i = 0; i < newNavigationBarItems.length; i++) {
       const navigationBarItem = {
-        name: props.navigationBarItemPageCombinations[i].navigationBarItem,
+        name: navigationBarItemPageCombinations[i].navigationBarItem,
         active:
-          props.navigationBarItemPageCombinations[i].navigationBarItem === activeNavigationBarItem
+          navigationBarItemPageCombinations[i].navigationBarItem === activeNavigationBarItem
             ? true
             : false
       }
       newNavigationBarItems[i] = navigationBarItem
     }
     setNavigationBarItems(newNavigationBarItems)
-  })
+  }
+
+  function handleChange(e) {
+    setState(props.navigationBarItemPageCombinations, e)
+  }
 
   return (
     <div>
-      <NavigationBar items={navigationBarItems} />
+      <NavigationBar items={navigationBarItems} onChange={handleChange} />
       <div id="page-wrapper">{props.navigationBarItemPageCombinations[0].page}</div>
     </div>
   )
