6c08726c11abcbe30212cbe367e2bf0fd51cae84
markuskreukniet <shonof@gmail.com>
2023-04-27 21:52:12 +0200

    WIP

--- src/renderer/src/components/ResultByFilesPage.jsx
+++ src/renderer/src/components/ResultByFilesPage.jsx
@@ -2,9 +2,15 @@ import FileOrFolderInput from '../FileOrFolderInput'
 import Page from './Page'
 
 export default function ResultByFilesPage(props) {
+  async function handleFilePaths(filePaths) {
+    props.onLoading(true)
+    props.handleFilePaths(filePaths)
+    props.onLoading(false)
+  }
+
   return (
     <Page title={props.title}>
-      <FileOrFolderInput onChange={props.handleFilePaths} />
+      <FileOrFolderInput onChange={handleFilePaths} />
       <h2>Result:</h2>
       {props.resultComponent}
     </Page>
