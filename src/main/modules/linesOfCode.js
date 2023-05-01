461a232940cd1605eb5c7fc88a1c06557d4eaa74
markuskreukniet <shonof@gmail.com>
2023-05-01 23:45:50 +0200

    linesOfCode

--- src/main/modules/linesOfCode.js
+++ src/main/modules/linesOfCode.js
@@ -1,4 +1,29 @@
+const fs = require('fs')
+const endOfLine = '\n'
+
 export default async function linesOfCode(filePaths) {
-  console.log(filePaths)
-  return 1
+  let result = 0
+
+  for (const path of filePaths) {
+    result += numberOfFileLinesWithoutCommentsAndEmptyLines(path)
+  }
+
+  return result
+}
+
+function numberOfFileLinesWithoutCommentsAndEmptyLines(path) {
+  let code = fs.readFileSync(path, { encoding: 'utf8' })
+  code = removeCommentsAndEmptyLines(code)
+  const lines = code.split(endOfLine)
+
+  return lines.length
+}
+
+function removeCommentsAndEmptyLines(code) {
+  code = code.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$|<!--(.|\s)*?-->/gm, '') // removes JavaScript, HTML, and CSS comments
+
+  let lines = code.split(endOfLine)
+  lines = lines.filter((line) => line.trim() !== '')
+
+  return lines.join(endOfLine)
 }
