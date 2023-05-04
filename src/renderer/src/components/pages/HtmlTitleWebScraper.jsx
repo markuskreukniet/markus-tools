59e299f4cdd6ec4241f7cd03d269c2fe61cb9512
markuskreukniet <shonof@gmail.com>
2023-05-04 15:51:38 +0200

    WIP

--- src/renderer/src/components/pages/HtmlTitleWebScraper.jsx
+++ src/renderer/src/components/pages/HtmlTitleWebScraper.jsx
@@ -1,5 +1,25 @@
 import ResultPage from '../ResultPage'
 
-export default function HtmlTitleWebScraper() {
-  return <ResultPage />
+export default function HtmlTitleWebScraper(props) {
+  const inputComponent = (
+    <div>
+      <div />
+    </div>
+  )
+
+  const outputComponent = (
+    <div>
+      <div />
+    </div>
+  )
+
+  return (
+    <ResultPage
+      title={props.title}
+      inputComponent={inputComponent}
+      outputComponent={outputComponent}
+      getOutput={function () {}}
+      onLoading={props.onLoading}
+    />
+  )
 }
