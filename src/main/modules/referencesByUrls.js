50e079c35ff1ddbc3d43b53ad8aec1be542a4f72
markuskreukniet <shonof@gmail.com>
2023-05-12 15:11:39 +0200

    WIP

--- src/main/modules/referencesByUrls.js
+++ src/main/modules/referencesByUrls.js
@@ -3,23 +3,38 @@ export default async function referencesByUrls(urlsString) {
   console.log('typeof urlsString', typeof urlsString)
 
   // AI result
-  const urls = urlsString.split(/\s+/).reduce((acc, val) => {
-    const urlsString = val.split(/https?:\/\//)
-    if (urlsString.length > 1) {
-      urlsString.forEach((url) => {
-        if (url.trim() !== '') {
-          acc.push(`https://${url}`)
-        }
-      })
-    } else {
-      const url = urlsString[0].trim()
-      if (url !== '') {
-        acc.push(url)
-      }
-    }
-    return acc
-  }, [])
+  // const urls = urlsString.split(/\s+/).reduce((acc, val) => {
+  //   const urlsString = val.split(/https?:\/\//)
+  //   if (urlsString.length > 1) {
+  //     urlsString.forEach((url) => {
+  //       if (url.trim() !== '') {
+  //         acc.push(`https://${url}`)
+  //       }
+  //     })
+  //   } else {
+  //     const url = urlsString[0].trim()
+  //     if (url !== '') {
+  //       acc.push(url)
+  //     }
+  //   }
+  //   return acc
+  // }, [])
 
-  console.log('urls', urls)
+  let testArray = splitWithSeparatorAsPrefixRecursion(urlsString, 'https://', [])
+  console.log('testArray', testArray)
+
   return 'testing'
+}
+
+function splitWithSeparatorAsPrefixRecursion(string, separator, array) {
+  const separatorIndex = string.indexOf(separator, separator.length)
+  const beforeSeparator = string.slice(0, separatorIndex)
+  const separatorAndAfterIt = string.slice(separatorIndex)
+  array.push(beforeSeparator)
+
+  if (separatorAndAfterIt.includes(separator, separator.length)) {
+    return splitWithSeparatorAsPrefixRecursion(separatorAndAfterIt, separator, array)
+  } else {
+    return array
+  }
 }
