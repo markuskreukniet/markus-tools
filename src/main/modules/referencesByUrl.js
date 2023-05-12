9d4b5d560df5401bf048cbf83d05d0d0e27daec8
markuskreukniet <shonof@gmail.com>
2023-05-12 13:04:27 +0200

    WIP

--- src/main/modules/referencesByUrl.js
+++ src/main/modules/referencesByUrl.js
@@ -1,4 +1,25 @@
-export default async function referencesByUrl(urls) {
-  console.log(urls)
+export default async function referencesByUrl(urlsString) {
+  console.log('urlsString', urlsString)
+  console.log('typeof urlsString', typeof urlsString)
+
+  // AI result
+  const urls = urlsString.split(/\s+/).reduce((acc, val) => {
+    const urlsString = val.split(/https?:\/\//)
+    if (urlsString.length > 1) {
+      urlsString.forEach((url) => {
+        if (url.trim() !== '') {
+          acc.push(`https://${url}`)
+        }
+      })
+    } else {
+      const url = urlsString[0].trim()
+      if (url !== '') {
+        acc.push(url)
+      }
+    }
+    return acc
+  }, [])
+
+  console.log('urls', urls)
   return 'testing'
 }
