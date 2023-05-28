cb2deb9fc6b50b70f732ba4066f8bc08759b7231
markuskreukniet <shonof@gmail.com>
2023-05-28 15:05:38 +0200

    WIP

--- src/main/modules/groupFilesByDateRange.js
+++ src/main/modules/groupFilesByDateRange.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
 import isNotAZeroByteFile from './fileHelper.js'
 
+// TODO: function looks a lot like duplicateFiles
 export default async function groupFilesByDateRange(filePaths) {
+  // TODO: also in duplicateFiles and check return type
+  if (filePaths.length < 2) {
+    return
+  }
+
   // path and date created combinations of files
   const pathDateCreatedCombinations = []
   for (const path of filePaths) {
@@ -17,8 +23,34 @@ export default async function groupFilesByDateRange(filePaths) {
     }
   }
 
-  // TODO: sort duplicate
   pathDateCreatedCombinations.sort(compare)
+
+  const groups = []
+  let group = [pathDateCreatedCombinations[0]]
+  for (let i = 1; i < pathDateCreatedCombinations.length; i++) {
+    const combination = pathDateCreatedCombinations[i - 1]
+    const combination2 = pathDateCreatedCombinations[i]
+
+    if (isWithinThreeDays(combination.birthtime, combination2.birthtime)) {
+      group.push(pathDateCreatedCombinations[i])
+    } else {
+      groups.push(group)
+      group = []
+    }
+  }
+
+  if (group.length > 0) {
+    groups.push(group)
+  }
+
+  console.log('groups', groups)
+}
+
+function isWithinThreeDays(date1, date2) {
+  const millisecondsDifference = Math.abs(date2.getTime() - date1.getTime())
+  const days = millisecondsDifference / (1000 * 3600 * 24)
+
+  return days <= 3
 }
 
 function compare(a, b) {
