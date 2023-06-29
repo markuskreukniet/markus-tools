a3207f094c27a22582ecb524ba2e00b1621a2394
markuskreukniet <shonof@gmail.com>
2023-06-29 14:58:38 +0200

    WIP

--- src/main/modules/filePaths.js
+++ src/main/modules/filePaths.js
@@ -1,7 +1,46 @@
-function getDirectoryTreeFilePaths(path, typeFilePaths, typeFileType) {
+import fs from 'fs'
+
+export async function getDirectoryFilePaths(path, directoryTree, typeFilePaths, typeFileType) {
   if (typeFilePaths === filePathsType.directories && typeFileType !== fileType.all) {
     return []
   }
+
+  const filePaths = []
+  const stack = [path]
+  while (stack.length > 0) {
+    const currentPath = stack.pop()
+
+    try {
+      const files = await fs.promises.readdir(currentPath)
+
+      const statsPromises = files.map((file) => {
+        const filePath = combinePathAndFile(currentPath, file)
+        return fs.promises.stat(filePath)
+      })
+
+      const stats = await Promise.all(statsPromises)
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i]
+        const filePath = combinePathAndFile(currentPath, file)
+        const fileStats = stats[i]
+
+        if (fileStats.isDirectory()) {
+          stack.push(filePath)
+        } else {
+          filePaths.push(filePath)
+        }
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return filePaths
+}
+
+function combinePathAndFile(path, file) {
+  return `${path}\\${file}`
 }
 
 export default function isNotAZeroByteFile(stats) {
