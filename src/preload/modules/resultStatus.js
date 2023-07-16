34bc679e5e3f9aab7fa099ab65cc504fe1ca984e
markuskreukniet <shonof@gmail.com>
2023-07-16 12:30:43 +0200

    WIP

--- src/preload/modules/resultStatus.js
+++ src/preload/modules/resultStatus.js
@@ -5,18 +5,33 @@ export const resultStatus = Object.freeze({
   errorSystem: 'errorSystem'
 })
 
+// toResultObject
 export function toResultObject(result, status, message) {
   return { result, status, message: message ? message : '' }
 }
 
+// TODO: remove
 export function toResultObjectWithNullResult(status, message) {
   return toResultObject(null, status, message)
+}
+
+export function toResultObjectWithNullResultAndResultStatusOk(message) {
+  return toResultObject(null, resultStatus.ok, message)
+}
+
+export function toResultObjectWithNullResultAndResultStatusErrorSystem(message) {
+  return toResultObject(null, resultStatus.errorSystem, message)
+}
+
+export function toResultObjectWithNullResultAndResultStatusPartiallyOk(message) {
+  return toResultObject(null, resultStatus.partiallyOk, message)
 }
 
 export function toResultObjectWithNullResultByResultObject(resultObject) {
   return toResultObject(null, resultObject.status, resultObject.message)
 }
 
+// isResultObject
 export function isResultObjectOk(resultObject) {
   return resultObject.status === resultStatus.ok
 }
