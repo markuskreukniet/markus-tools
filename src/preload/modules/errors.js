85460a94dd76f975e09c63b21d28cc8da91fba10
markuskreukniet <shonof@gmail.com>
2023-07-22 20:37:12 +0200

    WIP

--- src/preload/modules/errors.js
+++ src/preload/modules/errors.js
@@ -3,3 +3,16 @@ export const inputError = Object.freeze({
   wrongFunctionArguments: 'wrong function arguments',
   couldNotReadOrRemoveADirectory: 'Could not read or remove a directory'
 })
+
+export function ErrorTracker() {
+  const that = this
+  let errorCount = 0
+  let errorMessage = ''
+
+  this.concatErrorMessageOnNewLineAndIncrementErrorCount = function (errorMessage) {
+    that.errorCount++
+    that.errorMessage = `${that.errorMessage}\n${errorMessage}`
+  }
+
+  // TODO: getInstance() needed?
+}
