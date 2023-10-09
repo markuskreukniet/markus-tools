c63d6f3cefa6c7cc36a9bb274b66c2c0989a070e
markuskreukniet <shonof@gmail.com>
2023-10-09 15:11:22 +0200

    WIP

--- src/main/modules/synchronizeDirectory.js
+++ src/main/modules/synchronizeDirectory.js
@@ -6,33 +6,37 @@ export default async function synchronizeDirectory(
 ) {
   // TODO: this boolean should come from UI
   const directoriesTree = true
-
   // added getFileAndDirectoryFileObjects for synchronizeDirectory
-  const originalFileAndDirectoryFileObjectsRO = await getFileAndDirectoryFileObjects(
-    originalDirectoryFilePathObject.value,
-    directoriesTree
-  )
-  const destinationFileAndDirectoryFileObjectsRO = await getFileAndDirectoryFileObjects(
-    destinationDirectoryFilePathObject.value,
-    directoriesTree
-  )
 
-  const stack = [originalDirectoryFilePathObject]
+  const stack = [originalDirectoryFilePathObject.value]
   while (stack.length > 0) {
-    const currentPath = stack.pop()
-  }
+    const originalDirectoryPath = stack.pop()
+    const destinationDirectoryPath = originalDirectoryPath.replace(
+      originalDirectoryFilePathObject.value,
+      destinationDirectoryFilePathObject.value
+    )
 
-  for (const fileObject of originalFileAndDirectoryFileObjectsRO.result) {
-    if (fileObject.isDirectory) {
-      // if destination does not have the directory, copyDirectoryTree
-      await copyDirectoryTree()
-      // else stack.push(fileObject.path)
-    } else {
-      // if destination does not have the file
-      await copyFile()
-      // if destination does have the file and original file is newer, replace the file
-      // TODO: copyFile does replace?
-      await copyFile()
+    const originalFileAndDirectoryFileObjectsRO = await getFileAndDirectoryFileObjects(
+      originalDirectoryPath,
+      directoriesTree
+    )
+    const destinationFileAndDirectoryFileObjectsRO = await getFileAndDirectoryFileObjects(
+      destinationDirectoryPath,
+      directoriesTree
+    )
+
+    for (const fileObject of originalFileAndDirectoryFileObjectsRO.result) {
+      if (fileObject.isDirectory) {
+        // if destination does not have the directory, copyDirectoryTree
+        await copyDirectoryTree()
+        // else stack.push(fileObject.path)
+      } else {
+        // if destination does not have the file
+        await copyFile()
+        // if destination does have the file and original file is newer, replace the file
+        // TODO: copyFile does replace?
+        await copyFile()
+      }
     }
   }
 }
