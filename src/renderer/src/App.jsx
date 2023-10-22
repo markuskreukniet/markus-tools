a777ce3ce8bebf36c516f5d3f22f7b0b53b59c04
markuskreukniet <shonof@gmail.com>
2023-10-22 13:34:01 +0200

    added TODO

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -15,6 +15,7 @@ import PageNavigator from './components/PageNavigator'
 // TODO: rename files.js to filePath.js
 // TODO: rename openFileDialog to openFilePathDialog.js with functions
 // TODO: imagesToDateRangeFolder check input and output folder is not the same or a child
+// TODO: No Symlink Handling, which results in bugs
 
 // TODO: backend functions should return a object for:
 // function tagsAndMessageToJsx(openingTag, closingTag, message) {
