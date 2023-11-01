02815ba4a90cf243bdf1e97244cf54b92577c0e6
markuskreukniet <shonof@gmail.com>
2023-11-01 19:49:21 +0100

    added FileSelectionMode

--- go/internal/file_utils.go
+++ go/internal/file_utils.go
@@ -13,18 +13,33 @@ type FileDetail struct {
 	IsDirectory      bool
 }
 
-func getFileDetail(path string) (FileDetail, error) {
-	fileInfo, err := os.Stat(path)
+type FileSelectionMode int
+
+const (
+	files FileSelectionMode = iota
+	filesWithoutZeroByteFiles
+	filesAndDirectories
+	filesAndDirectoriesWithoutZeroByteFiles
+	directories
+)
+
+func getFileDetail(filePath string) (FileDetail, error) {
+	fileInfo, err := os.Stat(filePath)
 	if err != nil {
 		return FileDetail{}, err
 	}
 	return FileDetail{
-		Path:             path,
+		Path:             filePath,
 		ModificationTime: fileInfo.ModTime(),
 		Size:             fileInfo.Size(),
 		IsDirectory:      fileInfo.IsDir(),
 	}, nil
 }
+
+// directoryTree good naming? recursive is better?
+// func getAsdf(filePath string, directoryTree bool, fileSelectionMode FileSelectionMode) ([]FileDetail, error) {
+
+// }
 
 func joinOutputBasePathWithRelativeInputPath(inputBasePath, inputFullPath, outputBasePath string) (string, error) {
 	relativePath, err := filepath.Rel(inputBasePath, inputFullPath)
