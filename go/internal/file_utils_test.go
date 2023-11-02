5d5ec82c72e4c332586c18e7c38ab1bc854ccda7
markuskreukniet <shonof@gmail.com>
2023-11-02 22:24:31 +0100

    WIP

--- go/internal/file_utils_test.go
+++ go/internal/file_utils_test.go
@@ -3,7 +3,9 @@ package internal
 import (
 	"os"
 	"path/filepath"
+	"sort"
 	"testing"
+	"time"
 )
 
 func fatalLogIfError(t *testing.T, err error) {
@@ -63,6 +65,81 @@ func TestGetFileDetail(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("Expected an error when trying to get details of a non-existent file, but got none")
+	}
+}
+
+// TODO: WIP
+func TestGetFilteredFileDetailsFromDirectoryTree(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "testTempDir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	subDir := filepath.Join(tmpDir, "subDir")
+	file1 := filepath.Join(tmpDir, "file1.txt")
+	file2 := filepath.Join(tmpDir, "file2.txt")
+	file3 := filepath.Join(subDir, "file3.txt")
+
+	os.Mkdir(subDir, 0755)
+	os.WriteFile(file1, []byte("content"), 0666)
+	os.WriteFile(file2, []byte(""), 0666)
+	os.WriteFile(file3, []byte("content"), 0666)
+
+	modTime := time.Now().Add(-1 * time.Hour)
+	os.Chtimes(file1, modTime, modTime)
+	os.Chtimes(file2, modTime, modTime)
+	os.Chtimes(file3, modTime, modTime)
+
+	tests := []struct {
+		name           string
+		rootFilePath   string
+		fileFilterMode FileFilterMode
+		want           []FileDetail
+		wantErr        bool
+	}{
+		{
+			name:           "FilesOnly",
+			rootFilePath:   tmpDir,
+			fileFilterMode: files,
+			want: []FileDetail{
+				{Path: filepath.Join(tmpDir, "file1.txt"), Size: 7, IsDirectory: false},
+				{Path: filepath.Join(tmpDir, "file2.txt"), Size: 0, IsDirectory: false},
+				{Path: filepath.Join(subDir, "file3.txt"), Size: 7, IsDirectory: false},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getFilteredFileDetailsFromDirectoryTree(tt.rootFilePath, tt.fileFilterMode)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getFilteredFileDetailsFromDirectoryTree() error: %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			// If no error, check the content of the result
+			if !tt.wantErr && err == nil {
+				// Sort the slices by file path to ensure a consistent order for comparison.
+				sort.Slice(got, func(i, j int) bool { return got[i].Path < got[j].Path })
+				sort.Slice(tt.want, func(i, j int) bool { return tt.want[i].Path < tt.want[j].Path })
+
+				// Compare the length first.
+				if len(got) != len(tt.want) {
+					t.Fatalf("got %d file details; want %d", len(got), len(tt.want))
+				}
+
+				// Now compare the fields, ignoring the ModificationTime.
+				for i := range got {
+					if got[i].Path != tt.want[i].Path ||
+						got[i].Size != tt.want[i].Size ||
+						got[i].IsDirectory != tt.want[i].IsDirectory {
+						t.Errorf("getFilteredFileDetailsFromDirectoryTree() details mismatch; got %+v, want %+v", got[i], tt.want[i])
+					}
+				}
+			}
+		})
 	}
 }
 
