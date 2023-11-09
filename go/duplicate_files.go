cebc2bacbab41e7e365f9d6de53da4ad52adb7c6
markuskreukniet <shonof@gmail.com>
2023-11-09 15:45:34 +0100

    WIP

---
+++ go/duplicate_files.go
@@ -0,0 +1,14 @@
+package main
+
+type FileSystemNode struct {
+	FilePath    string
+	IsDirectory bool
+}
+
+// func duplicateFiles(uniqueFileSystemNodes []FileSystemNode) {
+// 	for _, value := range uniqueFileSystemNodes {
+// 		if !value.IsDirectory {
+// 			//
+// 		}
+// 	}
+// }
