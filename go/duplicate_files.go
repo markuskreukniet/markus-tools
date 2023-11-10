398f0d0a3f34fcf405a886b58076d8a776421ecb
markuskreukniet <shonof@gmail.com>
2023-11-11 00:08:33 +0100

    WIP

--- go/duplicate_files.go
+++ go/duplicate_files.go
@@ -1,23 +1,31 @@
 package main
 
-import "time"
-
 type FileSystemNode struct {
 	Path        string
 	IsDirectory bool
 }
 
 type FileIdentifier struct {
-	ModificationTime time.Time
-	Size             int64
-	Hash             string
+	Path string
+	Size int64
+	Hash string
 }
 
-// func duplicateFiles(uniqueFileSystemNodes []FileSystemNode) {
-// 	var FileIdentifiers []FileIdentifier
+// func duplicateFilesString(uniqueFileSystemNodes []FileSystemNode) (string, error) {
+// 	var fileIdentifiers []FileIdentifier
 // 	for _, value := range uniqueFileSystemNodes {
 // 		if !value.IsDirectory {
-// 			//
+// 			fileDetail, err := getFileDetail(value.Path)
+// 			if err != nil {
+// 				return "", err
+// 			}
+// 			fileIdentifiers = append(fileIdentifiers, FileIdentifier{
+// 				Path: fileDetail.Path,
+// 				Size: fileDetail.Size,
+// 				Hash: "",
+// 			})
+// 		} else {
+
 // 		}
 // 	}
 // }
