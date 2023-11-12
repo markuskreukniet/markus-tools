21daafcd023055893e65d0f079a445e0f35e6cd6
markuskreukniet <shonof@gmail.com>
2023-11-12 10:59:53 +0100

    WIP

--- go/duplicate_files.go
+++ go/duplicate_files.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 type FileSystemNode struct {
 	Path        string
 	IsDirectory bool
@@ -11,21 +13,38 @@ type FileIdentifier struct {
 	Hash string
 }
 
-// func duplicateFilesString(uniqueFileSystemNodes []FileSystemNode) (string, error) {
-// 	var fileIdentifiers []FileIdentifier
-// 	for _, value := range uniqueFileSystemNodes {
-// 		if !value.IsDirectory {
-// 			fileDetail, err := getFileDetail(value.Path)
-// 			if err != nil {
-// 				return "", err
-// 			}
-// 			fileIdentifiers = append(fileIdentifiers, FileIdentifier{
-// 				Path: fileDetail.Path,
-// 				Size: fileDetail.Size,
-// 				Hash: "",
-// 			})
-// 		} else {
+func appendFileIdentifier(fileIdentifiers *[]FileIdentifier, fileDetail FileDetail) []FileIdentifier {
+	return append(*fileIdentifiers, FileIdentifier{
+		Path: fileDetail.Path,
+		Size: fileDetail.Size,
+		Hash: "",
+	})
+}
 
-// 		}
-// 	}
-// }
+func duplicateFilesString(uniqueFileSystemNodes []FileSystemNode) (string, error) {
+	var fileIdentifiers []FileIdentifier
+	for _, value := range uniqueFileSystemNodes {
+		if !value.IsDirectory {
+			fileDetail, err := getFileDetail(value.Path)
+			if err != nil {
+				return "", err
+			}
+			if fileDetail.Size > 0 {
+				appendFileIdentifier(&fileIdentifiers, fileDetail)
+			}
+		} else {
+			err := walkFileDetails(value.Path, filesAndDirectoriesWithoutZeroByteFiles, func(fileDetail FileDetail) {
+				appendFileIdentifier(&fileIdentifiers, fileDetail)
+			})
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+	sort.Slice(fileIdentifiers, func(i, j int) bool {
+		return fileIdentifiers[i].Size < fileIdentifiers[j].Size
+	})
+	var duplicates []FileIdentifier
+	//
+	return "", nil
+}
