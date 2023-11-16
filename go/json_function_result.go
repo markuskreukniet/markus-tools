a91b5c2364f0f662959ab923af52c738cb99711e
markuskreukniet <shonof@gmail.com>
2023-11-16 20:20:02 +0100

    updated comment

--- go/json_function_result.go
+++ go/json_function_result.go
@@ -11,7 +11,7 @@ func jsonMarshalWithFallbackJSONError(nonJSON string) string {
 	jsonBytes, err := json.Marshal(nonJSON)
 	if err != nil {
 		// This JSON string should match FunctionResult.
-		// We can't use the error for the message since that might turn the string into an invalid JSON.
+		// We can't use error.Error() for the message since that might turn the string into an invalid JSON.
 		return `{"Result": null, "ErrorMessage": "json.Marshal error"}`
 	}
 	return string(jsonBytes)
