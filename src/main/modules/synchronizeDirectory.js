09643708ef66e2135dee68f954522453baa219be
markuskreukniet <shonof@gmail.com>
2023-11-24 11:22:41 +0100

    WIP stringsToGoFunctionCallWithArguments

--- src/main/modules/synchronizeDirectory.js
+++ src/main/modules/synchronizeDirectory.js
@@ -1,28 +1,34 @@
 import { exec } from 'child_process'
 import path from 'path'
 
+// TODO: fix errors, on close, and use stringsToGoFunctionCallWithArguments result
 export default async function synchronizeDirectory(sourceDirectory, destinationDirectory) {
-  const goDir = path.join(__dirname, '..', '..', 'go')
-
   const jsonArguments = JSON.stringify({
     sourceDirectory,
     destinationDirectory
   }).replace(/"/g, '\\"')
-  const goProcess = exec(
-    `go run . "synchronizeDirectoryTreesToJSON" "${jsonArguments}"`,
-    { cwd: goDir },
-    (error, stdout) => {
-      if (error) {
-        console.error(`Error executing Go program: ${error}`)
-        return
-      }
-      console.log(`Go program output: ${stdout}`)
-    }
-  )
-
-  goProcess.on('close', (code) => {
-    console.log(`Go program exited with code ${code}`)
-  })
+  await stringsToGoFunctionCallWithArguments('synchronizeDirectoryTreesToJSON', jsonArguments)
 
   return `${sourceDirectory} testB ${destinationDirectory}`
+}
+
+async function stringsToGoFunctionCallWithArguments(functionCall, jsonArguments) {
+  await new Promise((resolve, reject) => {
+    const goProcess = exec(
+      `go run . "${functionCall}" "${jsonArguments}"`,
+      { cwd: path.join(__dirname, '..', '..', 'go') },
+      (error, stdout) => {
+        if (error) {
+          console.error(`Error executing Go program: ${error}`)
+          reject(error)
+          return
+        }
+        console.log(`Go program output: ${stdout}`)
+        resolve(stdout)
+      }
+    )
+    goProcess.on('close', (code) => {
+      console.log(`Go program exited with code ${code}`)
+    })
+  })
 }
