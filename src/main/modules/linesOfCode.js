e6d79b13d69a5865cc07ddc92cb9d5bb27205c24
markuskreukniet <shonof@gmail.com>
2024-01-16 13:32:41 +0100

    linesOfCode refactor

--- src/main/modules/linesOfCode.js
+++ src/main/modules/linesOfCode.js
@@ -1,28 +1,19 @@
 import { readFileSync } from 'node:fs'
-import { Either, toEitherLeftResult, toEitherRightResult } from '../../preload/monads/either'
 import { removeHtmlCssJavaScriptComments } from './modifyString.js'
+import { toEitherLeftResult, toEitherRightResult } from '../../preload/monads/either'
 
 export default function linesOfCode(filePaths) {
   let numberOfLines = 0
   // TODO: are not filePaths?
   for (const path of filePaths) {
-    const result = numberOfFileLinesWithoutCommentsAndEmptyLines(path.path)
-    if (result.isRight()) {
-      numberOfLines += result.value
-    } else {
-      return toEitherLeftResult(result)
+    let content = ''
+    try {
+      content = readFileSync(path.path, { encoding: 'utf8' })
+    } catch (error) {
+      return toEitherLeftResult(error.message)
     }
+    content = removeHtmlCssJavaScriptComments(content)
+    numberOfLines += content.split('\n').filter((line) => line.trim() !== '').length
   }
   return toEitherRightResult(numberOfLines)
-}
-
-function numberOfFileLinesWithoutCommentsAndEmptyLines(filePath) {
-  let content = ''
-  try {
-    content = readFileSync(filePath, { encoding: 'utf8' })
-  } catch (error) {
-    return Either.left(error.message)
-  }
-  content = removeHtmlCssJavaScriptComments(content)
-  return Either.right(content.split('\n').filter((line) => line.trim() !== '').length)
 }
