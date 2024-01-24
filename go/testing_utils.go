a7ac7ece86710883ea81d7442983823f120d22e8
markuskreukniet <shonof@gmail.com>
2024-01-24 16:16:35 +0100

    moved test arrange vars

--- go/testing_utils.go
+++ go/testing_utils.go
@@ -8,30 +8,6 @@ import (
 	"testing"
 )
 
-// TODO: should move to arrange utils?
-// TODO: there are no files in the root (temp dir)
-var (
-	directoryEmpty               = "directory empty"
-	directory1                   = "directory 1"
-	directory2                   = "directory 2"
-	directory2WithDirectoryEmpty = filepath.Join(directory2, directoryEmpty)
-	directory2WithDirectory3     = filepath.Join(directory2, "directory 3")
-	directory2WithDirectory4     = filepath.Join(directory2, "directory 4")
-
-	txtFile1 = filepath.Join(directory1, "file 1.txt")
-	txtFile2 = filepath.Join(directory1, "file 2.txt")
-	txtFile3 = filepath.Join(directory2WithDirectory3, "file 3.txt")
-	txtFile4 = filepath.Join(directory2WithDirectory3, "file 4.txt")
-	txtFile5 = filepath.Join(directory2WithDirectory3, "file 5.txt")
-	txtFile6 = filepath.Join(directory2WithDirectory4, "file 6.txt")
-
-	jpgFile4 = filepath.Join(directory1, "file 4.jpg")
-
-	txtFileNonExistent1 = "non existent 1.txt"
-
-	emptyPathEndParts []string
-)
-
 func testingWriteFileContentWithContentAndIndex(t *testing.T, filePath string, index int) string {
 	t.Helper()
 	writtenContent := fmt.Sprintf("content %d", index)
