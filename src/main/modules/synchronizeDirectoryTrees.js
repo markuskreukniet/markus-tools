9fd061b73155c42231fc6dd744e6d4ee67587dd7
markuskreukniet <shonof@gmail.com>
2024-02-07 10:32:16 +0100

    WIP refactor

--- src/main/modules/synchronizeDirectoryTrees.js
+++ src/main/modules/synchronizeDirectoryTrees.js
@@ -1,43 +1,8 @@
-import { toEitherLeftResult, toEitherRightResult } from '../../preload/monads/either'
-import { exec } from 'child_process'
-import path from 'path'
+import { objectToJSONAndStringsToGoFunctionCallWithArguments } from './utils/utils'
 
 export default async function synchronizeDirectoryTrees(sourceDirectory, destinationDirectory) {
-  // TODO: does the replace work on systems besides Windows?
-  const jsonArguments = JSON.stringify({
+  return objectToJSONAndStringsToGoFunctionCallWithArguments('synchronizeDirectoryTreesToJSON', {
     sourceDirectory,
     destinationDirectory
-  }).replace(/"/g, '\\"')
-  const result = JSON.parse(
-    await stringsToGoFunctionCallWithArguments('synchronizeDirectoryTreesToJSON', jsonArguments)
-  )
-
-  // TODO: make function for this
-  if (result.ErrorMessage === '') {
-    return toEitherRightResult(result.Result)
-  } else {
-    return toEitherLeftResult(result.ErrorMessage)
-  }
-}
-
-async function stringsToGoFunctionCallWithArguments(functionCall, jsonArguments) {
-  return new Promise((resolve, reject) => {
-    let result = ''
-    const goProcess = exec(`go run . "${functionCall}" "${jsonArguments}"`, {
-      cwd: path.join(__dirname, '..', '..', 'go')
-    })
-    goProcess.stdout.on('data', (data) => {
-      result += data
-    })
-    goProcess.on('error', (error) => {
-      reject(error)
-    })
-    goProcess.on('close', (code) => {
-      if (code !== 0) {
-        reject(new Error(`Go process exited with code ${code}`))
-      } else {
-        resolve(result)
-      }
-    })
   })
 }
---
+++ src/main/modules/utils/utils.js
@@ -0,0 +1,42 @@
+import { exec } from 'child_process'
+import path from 'path'
+import { toEitherLeftResult, toEitherRightResult } from '../../../preload/monads/either'
+
+export async function objectToJSONAndStringsToGoFunctionCallWithArguments(
+  functionCall,
+  nonJSONObject
+) {
+  const result = JSON.parse(
+    await stringsToGoFunctionCallWithArguments(
+      functionCall,
+      JSON.stringify(nonJSONObject).replace(/"/g, '\\"') // TODO: does the replace work on systems besides Windows?
+    )
+  )
+  if (result.ErrorMessage === '') {
+    return toEitherRightResult(result.Result)
+  } else {
+    return toEitherLeftResult(result.ErrorMessage)
+  }
+}
+
+async function stringsToGoFunctionCallWithArguments(functionCall, jsonArguments) {
+  return new Promise((resolve, reject) => {
+    let result = ''
+    const goProcess = exec(`go run . "${functionCall}" "${jsonArguments}"`, {
+      cwd: path.join(__dirname, '..', '..', 'go')
+    })
+    goProcess.stdout.on('data', (data) => {
+      result += data
+    })
+    goProcess.on('error', (error) => {
+      reject(error)
+    })
+    goProcess.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Go process exited with code ${code}`))
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
