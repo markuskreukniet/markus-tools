1d5492be72d335a2399a2a9bb1275fb55a04a70e
markuskreukniet <shonof@gmail.com>
2024-02-08 11:53:05 +0100

    removed unused files

--- src/main/modules/plainTextFilesToText.js
+++
@@ -1,9 +0,0 @@
-import { objectToJSONAndStringsToGoFunctionCallWithArguments } from './utils/utils'
-
-// TODO: is async needed? same for synchronizeDirectoryTrees
-// TODO: use same function as synchronizeDirectoryTrees?
-export default async function plainTextFilesToText(uniqueFileSystemNodes) {
-  return objectToJSONAndStringsToGoFunctionCallWithArguments('plainTextFilesToTextToJSON', {
-    uniqueFileSystemNodes
-  })
-}
