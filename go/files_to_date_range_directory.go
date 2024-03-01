bfc98b36c3a340538aad8d69939c404faa82dea7
markuskreukniet <shonof@gmail.com>
2024-03-01 16:53:16 +0100

    WIP

--- go/files_to_date_range_directory.go
+++ go/files_to_date_range_directory.go
@@ -13,7 +13,14 @@ type fileModificationTime struct {
 	modificationTime time.Time
 }
 
+type dateRangeDirectory struct {
+	path      string
+	startDate string
+	endDate   string
+}
+
 func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, destinationDirectory string) error {
+	// TODO: should be []fileModificationTime
 	var paths []string
 	if err := utils.AppendFileDetails(
 		func(detail utils.FileDetail) {
@@ -21,14 +28,15 @@ func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, des
 		}, uniqueFileSystemNodes, utils.FilesWithoutZeroByteFiles); err != nil {
 		return err
 	}
-	var fileModificationTimes []fileModificationTime
+	var dateRangeDirectories []dateRangeDirectory
 	// TODO: AppendFileDetails should now not look into subdirectories.
 	// TODO: utils.Directories is changed from directories
 	if err := utils.AppendFileDetails(
 		func(detail utils.FileDetail) {
-			fileModificationTimes = append(fileModificationTimes, fileModificationTime{
-				path:             detail.Path,
-				modificationTime: detail.ModificationTime,
+			dateRangeDirectories = append(dateRangeDirectories, dateRangeDirectory{
+				path:      detail.Path,
+				startDate: "",
+				endDate:   "",
 			})
 		}, []utils.FileSystemNode{{
 			Path:        destinationDirectory,
@@ -37,23 +45,32 @@ func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, des
 		return err
 	}
 
-	// Remove the directories that are not a date range directory in the destination directory.
+	// Remove the directories from the slice that are not a date range directory in the destination directory.
 	const spacedHyphen = " - "
-	for i := 0; i < len(fileModificationTimes); {
-		base := filepath.Base(fileModificationTimes[i].path)
+	for i := 0; i < len(dateRangeDirectories); {
+		base := filepath.Base(dateRangeDirectories[i].path)
+		startDate := ""
+		endDate := ""
 		remove := false
 		if strings.Contains(base, spacedHyphen) {
 			baseParts := strings.Split(base, spacedHyphen)
-			if !isValidDateFormat(baseParts[0]) || !isValidDateFormat(baseParts[1]) {
+			if isValidDateFormat(baseParts[0]) && isValidDateFormat(baseParts[1]) {
+				startDate = baseParts[0]
+				endDate = baseParts[1]
+			} else {
 				remove = true
 			}
-		} else if !isValidDateFormat(base) {
+		} else if isValidDateFormat(base) {
+			startDate = base
+		} else {
 			remove = true
 		}
 		if remove {
-			fileModificationTimes[i] = fileModificationTimes[len(fileModificationTimes)-1]
-			fileModificationTimes = fileModificationTimes[:len(fileModificationTimes)-1]
+			dateRangeDirectories[i] = dateRangeDirectories[len(dateRangeDirectories)-1]
+			dateRangeDirectories = dateRangeDirectories[:len(dateRangeDirectories)-1]
 		} else {
+			dateRangeDirectories[i].startDate = startDate
+			dateRangeDirectories[i].endDate = endDate
 			i++
 		}
 	}
