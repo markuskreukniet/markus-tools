506446b1266195e54fb32489de0fb2077d75c07b
markuskreukniet <shonof@gmail.com>
2024-03-07 14:46:04 +0100

    updated comment

--- go/files_to_date_range_directory.go
+++ go/files_to_date_range_directory.go
@@ -29,7 +29,7 @@ func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, des
 	}
 
 	// Remove the directories from the slice that are not a date range directory in the destination directory.
-	// And possibly append filePathsTimeModified from the destination directory.
+	// And append the filePathsTimeModified from a date range directory.
 	const spacedHyphen = " - "
 	for i := 0; i < len(dateRangeDirectoryPaths); {
 		base := filepath.Base(dateRangeDirectoryPaths[i])
