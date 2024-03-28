bbcd736c906b3570c984b323a36a16511958924a
markuskreukniet <shonof@gmail.com>
2024-03-28 11:07:36 +0100

    WIP

--- go/files_to_date_range_directory_test.go
+++ go/files_to_date_range_directory_test.go
@@ -1,25 +1,85 @@
 package main
 
 import (
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/markuskreukniet/markus-tools/go/utils/test"
 )
 
+type plainTextFile struct {
+	name    string
+	content string
+}
+
+type directoryWithOptionalFile struct {
+	path          string
+	timeModified  time.Time
+	plainTextFile *plainTextFile
+}
+
+func createDirectoriesWithOptionalFile(directoriesWithOptionalFileAsDelimitedSemicolonString string) []directoryWithOptionalFile {
+	var directoriesWithOptionalFile []directoryWithOptionalFile
+
+	//
+	directoriesWithOptionalFileAsDelimitedSemicolonString = strings.TrimSuffix(directoriesWithOptionalFileAsDelimitedSemicolonString, ";")
+	directoriesWithOptionalFileAsDelimitedCommaString := strings.Split(directoriesWithOptionalFileAsDelimitedSemicolonString, ";")
+	for _, delimitedCommaString := range directoriesWithOptionalFileAsDelimitedCommaString {
+		directoryWithOptionalFileAsStrings := strings.Split(strings.TrimSpace(delimitedCommaString), ",")
+		path := ""
+		if directoryWithOptionalFileAsStrings[0] != "" {
+			path = directoryWithOptionalFileAsStrings[0]
+		}
+		timeModified := time.Now()
+		// TODO: timeModified from string
+		var plainTextFile *plainTextFile = nil
+		if directoryWithOptionalFileAsStrings[2] != "" {
+			plainTextFile.name = directoryWithOptionalFileAsStrings[2]
+			plainTextFile.content = ""
+			if directoryWithOptionalFileAsStrings[3] != "" {
+				plainTextFile.content = directoryWithOptionalFileAsStrings[3]
+			}
+		}
+		directoriesWithOptionalFile = append(directoriesWithOptionalFile, directoryWithOptionalFile{
+			path:          path,
+			timeModified:  timeModified,
+			plainTextFile: plainTextFile,
+		})
+	}
+
+	//
+	// for _, directoryWithOptionalFile := range directoriesWithOptionalFile {
+	// 	if directoryWithOptionalFile.path != "" {
+
+	// 	}
+	// }
+
+	return directoriesWithOptionalFile
+}
+
 func TestFilesToDateRangeDirectory(t *testing.T) {
 	// arrange
-	// source/empty
-	// source/directory 1/txt 1.txt
-	// source/directory 1/jpg 1.jpg
-
-	// source/directory 1/jpg 1.jpg
+	// inputAsDelimitedString := `
+	// 	,,txt 0.txt,;
+	// 	empty,,nil,;
+	// 	directory 1,,txt 1.txt,;
+	// 	directory 1,,jpg 1.jpg,;
+	// 	directory 2/directory 3,,txt 2 3.txt,;
+	// `
+	// destinationInputAsDelimitedString := `
+	// 	,,txt 0.txt,;
+	// 	2020-01-20,,nil,;
+	// 	2020-02-20,,txt 02.txt,;
+	// 	2020-03-20,,nil,;
+	// 	2020-04-20 - 2020-04-21,,nil,;
+	// 	2020-05-20 - 2020-05-21,,txt 05.txt,;
+	// 	2020-06-20 - 2020-06-21,,nil,;
+	// `
+	// inputDirectoriesWithOptionalFile := createDirectoriesWithOptionalFile(inputAsDelimitedString)
+	// destinationInputDirectoriesWithOptionalFile := createDirectoriesWithOptionalFile(destinationInputAsDelimitedString)
 
 	// TODO: duplicate naming of files
-
-	fileSystemPathEndParts := test.FileSystemPathEndParts{
-		DirectoryPathEndParts: []string{test.Directory1, test.Directory2WithDirectory3, test.Directory2WithDirectory4},
-		FilePathEndParts:      []string{test.TxtFile1, test.TxtFile3, test.TxtFile6, test.JpgFile4},
-	}
 
 	testCases := []struct {
 		metadata test.TestCaseMetadata
@@ -35,8 +95,8 @@ func TestFilesToDateRangeDirectory(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.metadata.Name, func(t *testing.T) {
 			// arrange and teardown
-			directoryTree := test.TestingCreateTempFileSystemStructureOrGetEmptyString(t, fileSystemPathEndParts)
-			defer test.TestingRemoveDirectoryTree(t, directoryTree)
+			// directoryTree := test.TestingCreateTempFileSystemStructureOrGetEmptyString(t, fileSystemPathEndParts)
+			// defer test.TestingRemoveDirectoryTree(t, directoryTree)
 
 			// act
 
