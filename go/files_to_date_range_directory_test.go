0d990b2e56d0105c5d1f9616d4e4c2fedc46bf52
markuskreukniet <shonof@gmail.com>
2024-04-02 15:40:03 +0200

    WIP

--- go/files_to_date_range_directory_test.go
+++ go/files_to_date_range_directory_test.go
@@ -10,19 +10,19 @@ func TestFilesToDateRangeDirectory(t *testing.T) {
 	// arrange
 	// inputAsDelimitedString := `
 	// 	,,txt 0.txt,;
-	// 	empty,,nil,;
+	// 	empty,,,;
 	// 	directory 1,,txt 1.txt,;
 	// 	directory 1,,jpg 1.jpg,;
 	// 	directory 2/directory 3,,txt 2 3.txt,;
 	// `
 	// destinationInputAsDelimitedString := `
 	// 	,,txt 0.txt,;
-	// 	2020-01-20,,nil,;
+	// 	2020-01-20,,,;
 	// 	2020-02-20,,txt 02.txt,;
-	// 	2020-03-20,,nil,;
-	// 	2020-04-20 - 2020-04-21,,nil,;
+	// 	2020-03-20,,,;
+	// 	2020-04-20 - 2020-04-21,,,;
 	// 	2020-05-20 - 2020-05-21,,txt 05.txt,;
-	// 	2020-06-20 - 2020-06-21,,nil,;
+	// 	2020-06-20 - 2020-06-21,,,;
 	// `
 	// inputDirectoriesWithOptionalFile := createDirectoriesWithOptionalFile(inputAsDelimitedString)
 	// destinationInputDirectoriesWithOptionalFile := createDirectoriesWithOptionalFile(destinationInputAsDelimitedString)
@@ -49,6 +49,7 @@ func TestFilesToDateRangeDirectory(t *testing.T) {
 			// act
 
 			// assert
+
 		})
 	}
 }
