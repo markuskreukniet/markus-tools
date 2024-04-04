45a35bc8e9575c9bcc58c81485748ba9c0a33bc8
markuskreukniet <shonof@gmail.com>
2024-04-04 13:45:03 +0200

    cleanup

--- go/synchronize_directory_trees_test.go
+++ go/synchronize_directory_trees_test.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/markuskreukniet/markus-tools/go/utils"
 	"github.com/markuskreukniet/markus-tools/go/utils/test"
@@ -49,16 +46,7 @@ func testingHaveDirectoryTreesSameFilePaths(t *testing.T, sourceDirectory, desti
 	return haveSameFilePaths
 }
 
-func testingContainsTxtFile4(stringSlice []string) bool {
-	for _, item := range stringSlice {
-		if item == test.TxtFile4 {
-			return true
-		}
-	}
-	return false
-}
-
-func TestSynchronizeDirectoryTrees2(t *testing.T) {
+func TestSynchronizeDirectoryTrees(t *testing.T) {
 	// arrange
 	newContent := "content directory 2/directory\ncontent 3 2-3 2 new"
 	input := `
@@ -67,6 +55,8 @@ func TestSynchronizeDirectoryTrees2(t *testing.T) {
 		directory 1,,txt 1.txt,;
 		directory 2/directory 3,,txt 2-3.txt,;
 	`
+
+	// Some file systems have a resolution of one second, so the file should be at least a second newer.
 	sourceInput := input + `
 		,,jpg 0.jpg,;
 		directory 2/empty,,,;
@@ -132,90 +122,6 @@ func TestSynchronizeDirectoryTrees2(t *testing.T) {
 					t.Fatalf("Failed to read file: %s", err)
 				}
 				test.TestingAssertEqualStrings(t, string(content), tc.updatedFile.content)
-			}
-		})
-	}
-}
-
-func TestSynchronizeDirectoryTrees(t *testing.T) {
-	// arrange
-	sourceFileSystemPathEndParts := test.FileSystemPathEndParts{
-		DirectoryPathEndParts: []string{test.DirectoryEmpty, test.Directory1, test.Directory2WithDirectoryEmpty, test.Directory2WithDirectory3},
-		FilePathEndParts:      []string{test.TxtFile1, test.TxtFile4, test.TxtFile5},
-	}
-	destinationFileSystemPathEndParts := test.FileSystemPathEndParts{
-		DirectoryPathEndParts: []string{test.DirectoryEmpty, test.Directory2WithDirectory3},
-		FilePathEndParts:      []string{test.TxtFile3, test.TxtFile4},
-	}
-
-	testCases := []struct {
-		metadata                          test.TestCaseMetadata
-		sourceFileSystemPathEndParts      test.FileSystemPathEndParts
-		destinationFileSystemPathEndParts test.FileSystemPathEndParts
-		wantSameFilePaths                 bool
-	}{
-		{
-			metadata:                          test.TestingCreateTestCaseMetadataWithNameBasicAndWantErrFalse(),
-			sourceFileSystemPathEndParts:      sourceFileSystemPathEndParts,
-			destinationFileSystemPathEndParts: destinationFileSystemPathEndParts,
-			wantSameFilePaths:                 true,
-		},
-		{
-			metadata:                          test.TestingCreateTestCaseMetadataWithWantErrTrue("Empty DestinationPathEndParts"),
-			sourceFileSystemPathEndParts:      sourceFileSystemPathEndParts,
-			destinationFileSystemPathEndParts: test.EmptyFileSystemPathEndParts,
-			wantSameFilePaths:                 false,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.metadata.Name, func(t *testing.T) {
-			// arrange and teardown
-			sourceDirectory := test.TestingCreateTempFileSystemStructureOrGetEmptyString(t, tc.sourceFileSystemPathEndParts)
-			defer test.TestingRemoveDirectoryTree(t, sourceDirectory)
-			destinationDirectory := test.TestingCreateTempFileSystemStructureOrGetEmptyString(t, tc.destinationFileSystemPathEndParts)
-			defer test.TestingRemoveDirectoryTree(t, destinationDirectory)
-
-			// Some file systems have a resolution of one second, so we must wait a second.
-			filePathTxtFile4 := ""
-			writtenContent := ""
-			if sourceDirectory != "" && destinationDirectory != "" && testingContainsTxtFile4(tc.sourceFileSystemPathEndParts.FilePathEndParts) && testingContainsTxtFile4(tc.destinationFileSystemPathEndParts.FilePathEndParts) {
-				filePathTxtFile4 = filepath.Join(destinationDirectory, test.TxtFile4)
-				test.TestingWriteFileContentWithContentAndIndex(t, filePathTxtFile4, 1)
-				time.Sleep(time.Second)
-				writtenContent = test.TestingWriteFileContentWithContentAndIndex(t, filepath.Join(sourceDirectory, test.TxtFile4), 2)
-			}
-
-			// act
-			err := synchronizeDirectoryTrees(sourceDirectory, destinationDirectory)
-
-			// assert
-			test.TestingAssertErrorToWantError(t, err, tc.metadata.WantErr)
-			haveSameFilePaths := testingHaveDirectoryTreesSameFilePaths(t, sourceDirectory, destinationDirectory)
-			if tc.wantSameFilePaths && !haveSameFilePaths {
-				t.Errorf("The source and destination directory trees do not have the same file paths.")
-			}
-			haveSameFilePaths = testingHaveDirectoryTreesSameFilePaths(t, destinationDirectory, sourceDirectory)
-			if tc.wantSameFilePaths && !haveSameFilePaths {
-				t.Errorf("The destination and source directory trees do not have the same file paths.")
-			}
-			if filePathTxtFile4 != "" {
-				file, err := os.Open(filePathTxtFile4)
-				if err != nil {
-					t.Errorf("Failed to open file: %v", err)
-				}
-				defer file.Close()
-				var builder strings.Builder
-				scanner := bufio.NewScanner(file)
-				for scanner.Scan() {
-					test.TestingWriteString(t, scanner.Text(), &builder)
-				}
-				// TODO: if?
-				err = scanner.Err()
-				if err != nil {
-					t.Errorf("Failed to read file content: %v", err)
-				}
-				test.TestingAssertEqualStrings(t, builder.String(), writtenContent)
 			}
 		})
 	}
