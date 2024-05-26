9b0e96ae5f8e63c47897a28f96e88d19cae40622
markuskreukniet <shonof@gmail.com>
2024-05-26 19:28:02 +0200

    WIP

--- go/utils/utils.go
+++ go/utils/utils.go
@@ -20,21 +20,3 @@ func WriteTwoNewlineStrings(builder *strings.Builder) (int, error) {
 	}
 	return bytesWritten, nil
 }
-
-// DuplicateFileGroups
-type DuplicateFileGroup struct {
-	Identifier string
-	FilePaths  []string
-}
-
-type DuplicateFileGroups []DuplicateFileGroup
-
-func (groups DuplicateFileGroups) AppendByIdentifier(identifier, filePath string) bool {
-	for i, group := range groups {
-		if identifier == group.Identifier {
-			groups[i].FilePaths = append(groups[i].FilePaths, filePath)
-			return true
-		}
-	}
-	return false
-}
