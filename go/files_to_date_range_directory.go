47e06a49d1207a06cdbe0ef161ef98bec52ef47d
markuskreukniet <shonof@gmail.com>
2024-07-02 14:20:21 +0200

    cleaning

--- go/files_to_date_range_directory.go
+++ go/files_to_date_range_directory.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"sort"
@@ -205,8 +204,7 @@ func isDirectoryChild(filePath, childFilePath string) (bool, error) {
 	return !strings.HasPrefix(path, "..") && !strings.Contains(path, string(filepath.Separator)), nil
 }
 
-// TODO: WIP
-func filesToDateRangeDirectoryWIP(uniqueFileSystemNodes []utils.FileSystemNode, destinationDirectory string) error {
+func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, destinationDirectory string) error {
 	var files []utils.FileData
 	var goodDirectoryFilePaths []string
 	var badDirectoryFilePaths []string
@@ -347,116 +345,11 @@ func extractBaseAndJoinWithFilePath(filePathWithBase, filePath string) string {
 	return filepath.Join(filePath, filepath.Base(filePathWithBase))
 }
 
-func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, destinationDirectory string) error {
-	var filePathsTimeModified []filePathTimeModified
-	if err := appendFilePathsTimeModified(&filePathsTimeModified, uniqueFileSystemNodes); err != nil {
-		return err
-	}
-	var dateRangeDirectoryPaths []string
-
-	// Two error variables are needed because errJ might become an error while errI does not.
-	var errJ error
-	errI := utils.AppendFileDetails(
-		func(detail utils.FileDetail) {
-			if isValidDateRangeDirectory(detail.Path) {
-				dateRangeDirectoryPaths = append(dateRangeDirectoryPaths, detail.Path)
-				errJ = appendFilePathsTimeModified(&filePathsTimeModified, createDirectoryFileSystemNodeInSlice(detail.Path))
-			}
-		}, createDirectoryFileSystemNodeInSlice(destinationDirectory), utils.Directories)
-	if errI != nil {
-		return errI
-	}
-	if errJ != nil {
-		return errJ
-	}
-
-	sort.Slice(filePathsTimeModified, func(i, j int) bool {
-		return filePathsTimeModified[i].timeModified.Before(filePathsTimeModified[j].timeModified)
-	})
-	startDateRange := 0
-	for i := 0; i < len(filePathsTimeModified)-1; i++ {
-		// TODO: duplicate OR i + 1 or i+1
-		iPlusOne := i + 1
-		if isWithinThreeDays(filePathsTimeModified[iPlusOne].timeModified, filePathsTimeModified[i].timeModified) {
-			name, err := createDirectoryDateRangeName(filePathsTimeModified[startDateRange].timeModified, filePathsTimeModified[i].timeModified)
-			if err != nil {
-				return err
-			}
-			subDirectoryPath := filepath.Join(destinationDirectory, name)
-			foundIndex := -1
-			// TODO: add the missing filePathsTimeModified to the dir and remove subDirectoryPath from dateRangeDirectoryPaths in this loop?
-			for j, path := range dateRangeDirectoryPaths {
-				if path == subDirectoryPath {
-					foundIndex = j
-					break
-				}
-			}
-			if foundIndex >= 0 {
-				dateRangeDirectoryPaths[foundIndex] = dateRangeDirectoryPaths[len(dateRangeDirectoryPaths)-1]
-				dateRangeDirectoryPaths = dateRangeDirectoryPaths[:len(dateRangeDirectoryPaths)-1]
-			} else {
-				if err := utils.CreateDirectory(subDirectoryPath); err != nil {
-					return err
-				}
-			}
-			var lastPathElements []string
-			for j := startDateRange; j <= i; j++ {
-				base := filepath.Base(filePathsTimeModified[j].filePath)
-				for _, element := range lastPathElements {
-					if element == base {
-						return fmt.Errorf("wants to move two files with the same name, '%s' in the directory '%s'", base, subDirectoryPath)
-					}
-				}
-				fullFilePath := filepath.Join(subDirectoryPath, base)
-				exists, err := utils.FileOrDirectoryExists(fullFilePath)
-				if err != nil {
-					return err
-				}
-				if !exists {
-					if err := os.Rename(filePathsTimeModified[j].filePath, fullFilePath); err != nil {
-						return err
-					}
-				}
-
-				// Removing this check from the loop by extracting code to a function can result in an 'err != null' check in this loop since that function can return an error.
-				// Therefore, this current check results in less code.
-				if j < i {
-					lastPathElements = append(lastPathElements, base)
-				}
-			}
-			startDateRange = iPlusOne
-		}
-	}
-	for _, path := range dateRangeDirectoryPaths {
-		if err := os.Remove(path); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func formatDateAndWriteString(builder *strings.Builder, time time.Time) error {
 	if _, err := builder.WriteString(toDateFormat(time)); err != nil {
 		return err
 	}
 	return nil
-}
-
-func createDirectoryFileSystemNodeInSlice(path string) []utils.FileSystemNode {
-	return []utils.FileSystemNode{{
-		Path:        path,
-		IsDirectory: true,
-	}}
-}
-
-func appendFilePathsTimeModified(filePathsTimeModified *[]filePathTimeModified, uniqueFileSystemNodes []utils.FileSystemNode) error {
-	return utils.AppendFileDetails(
-		func(detail utils.FileDetail) {
-			*filePathsTimeModified = append(*filePathsTimeModified, filePathTimeModified{
-				filePath:     detail.Path,
-				timeModified: detail.ModificationTime,
-			})
-		}, uniqueFileSystemNodes, utils.FilesWithoutZeroByteFiles)
 }
 
 func toDateFormat(time time.Time) string {
