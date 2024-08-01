2a8d733516deb687ebe6e4cbd9af8a8abad29f81
markuskreukniet <shonof@gmail.com>
2024-08-02 00:01:45 +0200

    WIP comment

--- go/references_by_urls_test.go
+++ go/references_by_urls_test.go
@@ -2,6 +2,46 @@ package main
 
 // import "testing"
 
+// func toUncommentedHTMLElements(htmlDocument string) {
+// 	var htmlElements []string
+// 	var htmlElement []rune
+// 	var wantedNextRune rune
+// 	isCommentBuilding := false
+// 	isCommentActive := false
+// 	countToFindElementOrComment := 0
+
+// 	htmlDocument = strings.TrimSpace(htmlDocument)
+
+// 	// don't forget escaping \
+
+// 	for _, r := range htmlDocument {
+// 		if isCommentBuilding || isCommentActive {
+// 			if r == wantedNextRune {
+// 				if r == '!' {
+// 					wantedNextRune = '-'
+// 					countToFindElementOrComment++
+// 				} else if r == '-' {
+// 					if countToFindElementOrComment == 2 {
+// 						wantedNextRune = '-'
+// 					} else {
+// 						isCommentActive = true
+// 					}
+// 				}
+// 			} else {
+// 				isCommentBuilding = false
+// 				countToFindElementOrComment = 0
+// 			}
+// 		} else {
+// 			if r == '<' {
+// 				isCommentBuilding = true
+// 				wantedNextRune = '!'
+// 				countToFindElementOrComment++
+// 			}
+// 		}
+// 	}
+
+// }
+
 // func TestReferencesByURLs(t *testing.T) {
 
 // }
