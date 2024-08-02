00e6b14aa819b3c3973e97e0840ead9e1368c354
markuskreukniet <shonof@gmail.com>
2024-08-02 11:28:19 +0200

    WIP comment

--- go/references_by_urls_test.go
+++ go/references_by_urls_test.go
@@ -10,36 +10,58 @@ package main
 // 	isCommentActive := false
 // 	countToFindElementOrComment := 0
 
+// 	var filteredHTMLDocument []rune
+
 // 	htmlDocument = strings.TrimSpace(htmlDocument)
 
 // 	// don't forget escaping \
 
 // 	for _, r := range htmlDocument {
-// 		if isCommentBuilding || isCommentActive {
-// 			if r == wantedNextRune {
-// 				if r == '!' {
-// 					wantedNextRune = '-'
-// 					countToFindElementOrComment++
-// 				} else if r == '-' {
-// 					if countToFindElementOrComment == 2 {
-// 						wantedNextRune = '-'
-// 					} else {
-// 						isCommentActive = true
+// 		if isCommentActive {
+// 			if isCommentBuilding {
+// 				if r == wantedNextRune {
+// 					if r == '-' {
+// 						wantedNextRune = '>'
+// 					} else if r == '>' {
+// 						isCommentActive = false
 // 					}
 // 				}
 // 			} else {
-// 				isCommentBuilding = false
-// 				countToFindElementOrComment = 0
+// 				if r == '-' {
+// 					wantedNextRune = '-'
+// 				} else {
+// 					filteredHTMLDocument = append(filteredHTMLDocument, r)
+// 				}
 // 			}
 // 		} else {
-// 			if r == '<' {
-// 				isCommentBuilding = true
-// 				wantedNextRune = '!'
-// 				countToFindElementOrComment++
+// 			if isCommentBuilding {
+// 				if r == wantedNextRune {
+// 					if r == '!' {
+// 						wantedNextRune = '-'
+// 						countToFindElementOrComment++
+// 					} else if r == '-' {
+// 						if countToFindElementOrComment == 2 {
+// 							wantedNextRune = '-'
+// 							countToFindElementOrComment++
+// 						} else {
+// 							isCommentActive = true
+// 						}
+// 					}
+// 				} else {
+// 					isCommentBuilding = false
+// 					countToFindElementOrComment = 0
+// 				}
+// 			} else {
+// 				if r == '<' {
+// 					isCommentBuilding = true
+// 					wantedNextRune = '!'
+// 					countToFindElementOrComment++
+// 				} else {
+// 					filteredHTMLDocument = append(filteredHTMLDocument, r)
+// 				}
 // 			}
 // 		}
 // 	}
-
 // }
 
 // func TestReferencesByURLs(t *testing.T) {
