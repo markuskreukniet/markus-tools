251ed4bb879a5b05fd452f9c23925f7071f8eb89
markuskreukniet <shonof@gmail.com>
2024-09-10 14:58:10 +0200

    added comment

--- src/renderer/src/components/Loader.jsx
+++ src/renderer/src/components/Loader.jsx
@@ -1,3 +1,5 @@
 export default function Loader(props) {
   return <div id="loader" classList={{ displayNone: !props.loading }} />
 }
+
+// TODO: spinner is better naming since it does not load anything
