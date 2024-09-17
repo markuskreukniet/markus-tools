b7ab09c7c70f377e30c088ef7f667113680abeb6
markuskreukniet <shonof@gmail.com>
2024-09-17 23:54:30 +0200

    BEM and cleaning

--- src/renderer/src/components/Loader.jsx
+++ src/renderer/src/components/Loader.jsx
@@ -1,5 +1,5 @@
 export default function Loader(props) {
-  return <div id="loader" classList={{ displayNone: !props.loading }} />
+  return <div id="loader" classList={{ 'display-none': !props.loading }} />
 }
 
 // TODO: spinner is better naming since it does not load anything
