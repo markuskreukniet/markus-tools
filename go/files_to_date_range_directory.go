77ebd750da1b5a8a786cfdd442fb807c1240c12a
markuskreukniet <shonof@gmail.com>
2024-09-28 23:38:27 +0200

    CreateFileMetadata

--- go/files_to_date_range_directory.go
+++ go/files_to_date_range_directory.go
@@ -243,7 +243,7 @@ func appendPathsAndFilesByReadingDirectory(path string, handler func(string, str
 			*files = append(*files,
 				utils.CreateFileSystemFileExtra("",
 					utils.CreateFileSystemFile("",
-						utils.CreateFileMetadata(info.Name(), path, fullPath, info.ModTime(), info.Size(), false))))
+						utils.CreateFileMetadata(info.Name(), path, fullPath, "", info.ModTime(), info.Size(), false))))
 		}
 	}
 	return nil
