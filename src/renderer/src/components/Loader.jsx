e75b39ce72986c3e6fee577433d65d0d6ff37c49
markuskreukniet <shonof@gmail.com>
2024-10-02 22:33:10 +0200

    renaming Loader to LoadingSpinner

--- src/renderer/src/components/Loader.jsx
+++
@@ -1,5 +0,0 @@
-export default function Loader(props) {
-  return <div id="loader" classList={{ 'display-none': !props.loading }} />
-}
-
-// TODO: spinner is better naming since it does not load anything
