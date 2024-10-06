deb5160909128eb45327276a439c604da3027b57
markuskreukniet <shonof@gmail.com>
2024-10-07 00:04:50 +0200

    WIP

--- kotlin/src/main/kotlin/utils/FileUtils.kt
+++ kotlin/src/main/kotlin/utils/FileUtils.kt
@@ -1,13 +1,34 @@
 package org.example.utils
 
+import java.io.File
+
 data class FileSystemFile(val data: String, val fileMetadata: FileMetadata)
 
 data class FileMetadata(
   val name: String,
-  val directoryPath: String,
-  val path: String,
+  val absoluteDirectoryPath: String,
+  val absolutePath: String,
   val timeModified: Long,
   val size: Long,
   val isDirectory: Boolean,
   var hash: String,
 )
+
+// TODO: should the Golang version return a FileMetadata{} instead of an error?
+fun ToFileMetadata(absoluteFilePath: String): FileMetadata? {
+  val file = File(absoluteFilePath)
+
+  if (!file.exists()) {
+    return null
+  }
+
+  return FileMetadata(
+    name = file.name,
+    absoluteDirectoryPath = "",
+    absolutePath = absoluteFilePath,
+    timeModified = file.lastModified(), // TODO: format time
+    size = file.length(),
+    isDirectory = file.isDirectory(),
+    hash = ""
+  )
+}
