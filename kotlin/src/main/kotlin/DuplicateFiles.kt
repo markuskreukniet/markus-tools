ecdcee473c33cd5994396d358846e0f82bb7ead0
markuskreukniet <shonof@gmail.com>
2024-10-11 17:41:37 +0200

    WIP

--- kotlin/src/main/kotlin/DuplicateFiles.kt
+++ kotlin/src/main/kotlin/DuplicateFiles.kt
@@ -2,7 +2,9 @@ package org.example
 
 import org.example.utils.*
 
-fun getDuplicateFilesAsNewlineSeparatedString(uniqueFileSystemNodes: Array<FileSystemNode>): String {
+fun getDuplicateFilesAsNewlineSeparatedString(
+  uniqueFileSystemNodes: Array<FileSystemNode>
+): Result<String?> = runCatching {
   data class DuplicateFileMetadata(
     override val absolutePath: String,
     override val size: Long,
@@ -10,7 +12,7 @@ fun getDuplicateFilesAsNewlineSeparatedString(uniqueFileSystemNodes: Array<FileS
   ) : FileMetadata
 
   val result = StringBuilder()
-  val files = mutableListOf<DuplicateFileMetadata>()
+  val files = mutableListOf<FileMetadata>()
 
   val handler = fun(file: CompleteFileMetadata) {
     files.add(DuplicateFileMetadata(
@@ -24,7 +26,21 @@ fun getDuplicateFilesAsNewlineSeparatedString(uniqueFileSystemNodes: Array<FileS
     walkFilterAndHandleFileMetadata(node.absolutePath, FileFilterMode.NON_ZERO_BYTE_FILES, FileType.ALL_FILES, handler)
   }
 
-  result.append("test")
+  val groups = createFileMetadataByHashGroups(files, true).getOrThrow() ?: return@runCatching null
 
-  return result.toString()
+  groups.forEachIndexed { indexI, group ->
+    if (indexI > 0) {
+      writeTwoNewlineStrings(result)
+    }
+    group.forEachIndexed { indexJ, file ->
+      if (indexJ > 0) {
+        writeNewlineString(result)
+      }
+      result.append(file.absolutePath)
+    }
+  }
+
+  result.toString()
+
+  "test"
 }
