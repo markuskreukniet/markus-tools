f4df8b5522805606d70017b296c20094fcc63e47
markuskreukniet <shonof@gmail.com>
2024-10-27 18:00:34 +0100

    WIP

--- kotlin/src/main/kotlin/FilesToDateRangeDirectory.kt
+++ kotlin/src/main/kotlin/FilesToDateRangeDirectory.kt
@@ -18,7 +18,7 @@ fun isValidDateRangeDirectoryName(name: String): Boolean {
     val nameParts = name.split(spacedHyphen)
     val firstDate = parseDate(nameParts.first()).getOrElse { return false }
     val secondDate = parseDate(nameParts[1]).getOrElse { return false }
-    if (abs(ChronoUnit.DAYS.between(firstDate, secondDate)) in 1..3) {
+    if (abs(ChronoUnit.DAYS.between(firstDate, secondDate)) in 1..3) { // TODO: can be longer than three days
       return true
     }
   } else {
@@ -28,6 +28,16 @@ fun isValidDateRangeDirectoryName(name: String): Boolean {
   return false
 }
 
+fun appendSubtreeContents(directory: File, files: MutableList<File>, directories: MutableList<File>) {
+  directory.walk().drop(1).forEach { file ->
+    if (file.isDirectory) {
+      directories.add(file)
+    } else {
+      files.add(file)
+    }
+  }
+}
+
 fun categorizeFilesAndDirectories(
   destinationDirectory: File
 ): Result<Pair<MutableList<File>, Pair<MutableList<File>, MutableList<File>>>> = runCatching {
@@ -35,17 +45,27 @@ fun categorizeFilesAndDirectories(
   val goodDirectories = mutableListOf<File>()
   val badDirectories = mutableListOf<File>()
 
+  // TODO: naming
+  val test = fun(directories: MutableList<File>) {
+    directories.forEach { directory ->
+      appendSubtreeContents(directory, files, badDirectories)
+    }
+  }
+
   destinationDirectory.walk().maxDepth(1).forEach { file ->
-    if (file.isFile) {
-      files.add(file)
-    } else if (file.isDirectory) {
+    if (file.isDirectory) {
       if (isValidDateRangeDirectoryName(file.name)) {
         goodDirectories.add(file)
       } else {
         badDirectories.add(file)
       }
+    } else {
+      files.add(file)
     }
   }
+
+  test(goodDirectories)
+  test(badDirectories)
 
   Pair(files, Pair(goodDirectories, badDirectories))
 }
