dbead2c59056beec499dbd72acdcf7ae69dcce73
markuskreukniet <shonof@gmail.com>
2024-11-13 15:15:34 +0100

    bug fix

--- go/files_to_date_range_directory.go
+++ go/files_to_date_range_directory.go
@@ -393,7 +393,7 @@ func createHandlers(
 		badFiles *[]utils.DateRangeFileInfo, goodFiles *[]utils.DateRangeFileInfo, file utils.DateRangeFileInfo,
 	) {
 		*badFiles = append(*badFiles, *goodFiles...)
-		goodFiles = &[]utils.DateRangeFileInfo{file}
+		*goodFiles = []utils.DateRangeFileInfo{file}
 	}
 
 	categorizeOnShortestFileNameLength := func(
@@ -539,9 +539,9 @@ func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, des
 		categorize(info, node.Path, &filesNew, &badDirectoryFilePaths, addDirectory)
 	}
 
-	// if err := deleteDuplicateFiles(&filesNew, destinationDirectory); err != nil {
-	// 	return err
-	// }
+	if err := deleteDuplicateFiles(&filesNew, destinationDirectory); err != nil {
+		return err
+	}
 
 	// TODO: remove this converting
 	var files []utils.FileSystemFile
@@ -552,10 +552,10 @@ func filesToDateRangeDirectory(uniqueFileSystemNodes []utils.FileSystemNode, des
 		})
 	}
 
-	files, err = filterAndDeleteDuplicateFiles(files, destinationDirectory)
-	if err != nil {
-		return err
-	}
+	// files, err = filterAndDeleteDuplicateFiles(files, destinationDirectory)
+	// if err != nil {
+	// 	return err
+	// }
 
 	sort.Slice(files, func(i, j int) bool {
 		return files[i].FileMetadata.TimeModified.Before(files[j].FileMetadata.TimeModified)
