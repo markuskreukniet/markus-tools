2dae0709296e2b9cb9e1af0096f0fb5885111937
markuskreukniet <shonof@gmail.com>
2024-12-03 10:51:56 +0100

    refactor

--- go/plain_text_files_to_text.go
+++ go/plain_text_files_to_text.go
@@ -3,38 +3,10 @@ package main
 import (
 	"bufio"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/markuskreukniet/markus-tools/go/utils"
 )
-
-// WIP refactor
-
-func addBaseAndLines(filePath string, builder *strings.Builder) error {
-	if _, err := builder.WriteString(filepath.Base(filePath)); err != nil {
-		return err
-	}
-
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		if _, err := utils.WriteNewlineString(builder); err != nil {
-			return err
-		}
-		if _, err = builder.WriteString(scanner.Text()); err != nil {
-			return err
-		}
-	}
-
-	return scanner.Err()
-}
 
 func plainTextFilesToTextToJSON(uniqueFileSystemNodes []utils.FileSystemNode) string {
 	text, err := plainTextFilesToText(uniqueFileSystemNodes)
@@ -44,69 +16,64 @@ func plainTextFilesToTextToJSON(uniqueFileSystemNodes []utils.FileSystemNode) st
 	return resultToJSONFunctionResult(text)
 }
 
-// Opening a file two times is not the most efficient, but having a separate open file in isTextFile helps with filtering.
 func plainTextFilesToText(uniqueFileSystemNodes []utils.FileSystemNode) (string, error) {
-	// var files []utils.FTextFilesFileInfo
+	var files []utils.FTextFilesFileInfo
 
-	// handler := func(file utils.CompleteFileInfo) {
-	// 	files = append(files, utils.FTextFilesFileInfo{
-	// 		Name: file.Name,
-	// 		Path: file.Name,
-	// 	})
-	// }
-
-	// for _, node := range uniqueFileSystemNodes {
-	// 	if err :=
-	// 		utils.WalkFilterAndHandleFileInfo(node, utils.NonZeroByteFiles, utils.TextFiles, handler); err != nil {
-	// 		return "", err
-	// 	}
-	// }
-
-	//
-	var filePaths []string
+	handler := func(file utils.CompleteFileInfo) {
+		files = append(files, utils.FTextFilesFileInfo{
+			Name:         file.Name,
+			AbsolutePath: file.AbsolutePath,
+		})
+	}
 
 	for _, node := range uniqueFileSystemNodes {
-		if node.IsDirectory {
-			handler := func(file utils.FileSystemFile) error {
-				filePaths = append(filePaths, file.FileMetadata.Path)
-				return nil
-			}
-
-			if err := utils.WalkFilterAndHandleFileSystemFile(node.Path, utils.NonZeroByteFiles, utils.TextFiles, handler); err != nil {
-				return "", err
-			}
-		} else {
-			file, err := utils.ToFileSystemFile(node.Path)
-			if err != nil {
-				return "", err
-			}
-			if file.FileMetadata.Size > 0 {
-				isTextFile, err := utils.IsTextFile(file.FileMetadata.Path)
-				if err != nil {
-					return "", err
-				}
-				if isTextFile {
-					filePaths = append(filePaths, file.FileMetadata.Path)
-				}
-			}
+		if err :=
+			utils.WalkFilterAndHandleFileInfo(node, utils.NonZeroByteFiles, utils.TextFiles, handler); err != nil {
+			return "", err
 		}
 	}
 
-	var result strings.Builder
-	length := len(filePaths)
+	if len(files) == 0 {
+		return "", nil
+	}
 
-	if length > 0 {
-		err := addBaseAndLines(filePaths[0], &result)
+	var result strings.Builder
+
+	addNameAndLines := func(fileInfo utils.FTextFilesFileInfo) error {
+		if _, err := result.WriteString(fileInfo.Name); err != nil {
+			return err
+		}
+
+		file, err := os.Open(fileInfo.AbsolutePath)
 		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			if _, err := utils.WriteNewlineString(&result); err != nil {
+				return err
+			}
+			if _, err = result.WriteString(scanner.Text()); err != nil {
+				return err
+			}
+		}
+
+		return scanner.Err()
+	}
+
+	if err := addNameAndLines(files[0]); err != nil {
+		return "", err
+	}
+
+	for _, file := range files[1:] {
+		if _, err := utils.WriteTwoNewlineStrings(&result); err != nil {
 			return "", err
 		}
-		for _, path := range filePaths[1:] {
-			if _, err := utils.WriteTwoNewlineStrings(&result); err != nil {
-				return "", err
-			}
-			if err = addBaseAndLines(path, &result); err != nil {
-				return "", err
-			}
+		if err := addNameAndLines(file); err != nil {
+			return "", err
 		}
 	}
 
