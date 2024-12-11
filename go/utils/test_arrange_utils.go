dbfffe5eab95ebadf3a81af93ce3b7ae0dbe97d1
markuskreukniet <shonof@gmail.com>
2024-12-11 13:39:09 +0100

    fixed TODO

--- go/utils/test_arrange_utils.go
+++ go/utils/test_arrange_utils.go
@@ -75,7 +75,7 @@ func WriteFilesBySingleInput(t *testing.T, input string) string {
 		return ""
 	}
 
-	directoryPath := tMustCreateTemporaryDirectory(t) // TODO: also do it like this in Kotlin code
+	directoryPath := tMustCreateTemporaryDirectory(t)
 	files := createFilesData(t, directoryPath, input)
 
 	createdDirectoryPaths := make(map[string]struct{})
@@ -121,16 +121,17 @@ func WriteFilesByMultipleInputs(t *testing.T, input string) ([]string, []FileSys
 		}
 	}
 
-	previousSegment = "" // TODO: check if useless, also for Kotlin code
+	// previousSegment = "" is unnecessary since the possible coming assignments are temporary directory paths,
+	// which they were not before.
 	var temporaryDirectoryPaths []string
 	var fileSystemNodes []FileSystemNode
 
 	for _, group := range fileGroups {
-		directory := tMustCreateTemporaryDirectory(t)
-		temporaryDirectoryPaths = append(temporaryDirectoryPaths, directory)
+		directoryPath := tMustCreateTemporaryDirectory(t)
+		temporaryDirectoryPaths = append(temporaryDirectoryPaths, directoryPath)
 		for _, file := range group {
-			file.CompleteFileInfo.AbsoluteDirectoryPath = filepath.Join(directory, file.CompleteFileInfo.AbsoluteDirectoryPath)
-			file.CompleteFileInfo.AbsolutePath = filepath.Join(directory, file.CompleteFileInfo.AbsolutePath)
+			file.CompleteFileInfo.AbsoluteDirectoryPath = filepath.Join(directoryPath, file.CompleteFileInfo.AbsoluteDirectoryPath)
+			file.CompleteFileInfo.AbsolutePath = filepath.Join(directoryPath, file.CompleteFileInfo.AbsolutePath)
 			if file.CompleteFileInfo.AbsoluteDirectoryPath != previousSegment {
 				tMustCreateDirectoryAll(t, file.CompleteFileInfo.AbsoluteDirectoryPath)
 			}
@@ -225,15 +226,13 @@ func WriteFileWithContentAndIndex(t *testing.T, filePath string, index int) stri
 }
 
 func getFirstPathSegment(filePath string) string {
-	cleanPath := filepath.Clean(filePath) // TODO: is needed?
-
 	for {
-		parentPath := filepath.Dir(cleanPath)
-		if parentPath == "." || parentPath == cleanPath {
+		parentPath := filepath.Dir(filePath)
+		if parentPath == "." || parentPath == filePath {
 			break
 		}
-		cleanPath = parentPath
+		filePath = parentPath
 	}
 
-	return cleanPath
+	return filePath
 }
