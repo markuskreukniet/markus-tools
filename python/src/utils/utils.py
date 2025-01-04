3b3c3692aa20483f48bf24e25fe57605b30c98c6
Markus Kreukniet <shonof@gmail.com>
2025-01-04 10:52:39 +0100

    cleaning + use create_model_configuration

--- python/src/utils/utils.py
+++ python/src/utils/utils.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
 
+FILE_NAME = "\"fileName\":"
+SENTENCE1 = "The content of a text file follows."
+SENTENCE2 = "Give a good file name for that file in a JSON format."
+INSTRUCTION = f"{SENTENCE1} {SENTENCE2} The file name should be the property's {FILE_NAME} value as one string.\n\n"
+
 # TODO: is useless? use instead? if not s. And .strip() should not be part of it?
 def is_blank(s):
   return not s.strip()
 
 @dataclass
 class ModelConfiguration:
-  number_of_context_tokens: int # The model's maximum token limit includes input (prompt) and generated output. # TODO: remove: num_ctx
-  max_output_tokens: int # TODO: remove: num_predict
+  number_of_context_tokens: int # The model's maximum token limit includes input (prompt) and generated output.
+  max_output_tokens: int
 
   seed: int
   tfs_z: int
@@ -25,9 +30,8 @@ class ModelConfiguration:
   mirostat_eta: float
   mirostat_tau: float
 
-def create_model_configuration(number_of_input_tokens):
-  number_of_context_tokens = 2048 # TODO: duplicate
-
+# TODO: prompt should be part of model creation
+def create_model_configuration(number_of_context_tokens, number_of_input_tokens):
   return ModelConfiguration(
     number_of_context_tokens = number_of_context_tokens,
     max_output_tokens = number_of_context_tokens - number_of_input_tokens,
